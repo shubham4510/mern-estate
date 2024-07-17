8c390ee3485cb66d57e440b25d85dd16ebce37ff
Shubham <shubhambhardwaj112277@gmail.com>
2024-07-17 21:25:18 +0530

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Signin from './pages/Signin.jsx'
+import SignUp from './pages/SignUp.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/sign-in' element={<Signin/>} />
+            <Route path='/sign-up' element={<SignUp/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/profile' element={<Profile/>} />
+        </Routes>
+    </div>
   )
 }
 
