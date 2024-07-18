4b7b68ec28977742b3fe7a84ad1f6c24ed9282a7
Shubham <shubhambhardwaj112277@gmail.com>
2024-07-18 20:36:20 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDB from './config/database.js';
+import userRoutes from './routes/user.route.js'
 
 const app = express();
 
@@ -7,6 +8,8 @@ const app = express();
 app.listen(3000,()=>{
     console.log(`Server is running on port 3000`);
 
-})
+});
+
+app.use('/api/user', userRoutes);
 
 connectDB();
